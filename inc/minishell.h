60a445943598f09dad882aa9f5798fb89ddd427a
Mostafa Omrane <momrane@bess-f1r3s3.clusters.42paris.fr>
2024-03-13 10:48:58 +0100

    file renamed

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,20 +6,23 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:37:20 by momrane           #+#    #+#             */
-/*   Updated: 2024/03/12 16:30:51 by momrane          ###   ########.fr       */
+/*   Updated: 2024/03/13 10:21:41 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/inc/libft.h"
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include "../libft/inc/libft.h"
 
 # define YES 1
 # define NO 0
+
+/*		UTILS		*/
+int	ft_isspace(char c);
 
 #endif
