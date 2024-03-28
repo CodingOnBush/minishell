6cf3bc7fe5043fe7818d1f552cab1fb4f9767101
vvaudain <vvaudain@student.42.fr>
2024-03-28 11:10:12 +0100

    wip expand v3

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 12:16:46 by vvaudain          #+#    #+#             */
-/*   Updated: 2024/03/27 14:59:33 by vvaudain         ###   ########.fr       */
+/*   Updated: 2024/03/28 11:08:30 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,15 @@ int	ft_check_expands(t_token *list)
 	return (SUCCESS);
 }
 
-//On veut recuperer la ligne VAR --> on peut utiliser strncmp avec strlen de notre nom de var
+int	dist_to_quote(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] && str[i] != '\'')
+		i++;
+	return (i);
+}
 
 char	*ft_get_vars(char *str)
 {
@@ -79,27 +86,66 @@ char	*ft_get_vars(char *str)
 			free(tmp);
 			str += i;
 		}
+		else
+			return (ret_value);
 		if (str[i] == '\'')
 		{
-			i = dist_to_quote(*str); // coder la fonction dist_to_quote
-			ft_strlcpy(tmp, str, i);
-			ret_value = ft_strjoin(ret_value, tmp);
-			free(tmp);
-			str += i;
+			str++;
+			if (*str)
+			{
+				i = dist_to_quote(*str); // coder la fonction dist_to_quote
+				ft_strlcpy(tmp, str, i);
+				ret_value = ft_strjoin(ret_value, tmp);
+				free(tmp);
+				str += i;
+			}
+			else
+				return (ret_value);
 		}
 		else if (str[i] == '\"')
 		{
 			i++;
+			str++;
 			j = i;
 			while (str[j] && str[j] != '$' && str[j] != '\"')
 				j++;
-			str += j - i + 1;
+			if (j - i != 0 && str[j] != NULL)
+			{
+				ft_strlcpy(tmp, str, j - i + 1);
+				ret_value = ft_strjoin(ret_value, tmp);
+				free(tmp);
+				str += j - i + 1;
+			}
+			else
+				return (ret_value);
 			i = j;
 			if (str[j] && str[j] == '$')
 			{
-				while (str[j] && str[j] != '\"' && ft_isspace(str[j]) == NO))
+				while (str[j] && str[j] != '\"' && ft_isspace(str[j]) == NO)
 					j++;
-				if (str[j] != NULL && (str[j] == '\"' || ft_isspace(str[j]) == YES))
+				if (str[j] != NULL && ft_isspace(str[j]) == YES)
+				{
+					ft_strlcpy(tmp, str, j - i + 1);
+					var_value = ft_strdup(getenv((const)tmp));
+					ret_value = ft_strjoin(ret_value, var_value);
+					free(tmp);
+					free(var_value);
+					j++;
+					str += j;
+					i = j;
+					while (str[j] && str[j] != '\"')
+						j++;
+					if (j - i != 0 && str[j])
+					{
+						ft_strlcpy(tmp, str, j - i + 1);
+						ret_value = ft_strjoin(ret_value, tmp);
+						free(tmp);
+						str += j - i + 1;
+					}
+					str += j;
+					i = j;
+				}
+				else if (str[j] && str[j] == '\"')
 				{
 					ft_strlcpy(tmp, str, j - i + 1);
 					var_value = ft_strdup(getenv((const)tmp));
@@ -109,9 +155,17 @@ char	*ft_get_vars(char *str)
 					str += j;
 					i = j;
 				}
+				else
+					return (ret_value);
 			}
 			
 		}
 		else if (str[i] == '$')
+		{
+			i++;
+			while (str[i] && str[i] != '\"' && ft_isspace(str[i]) == NO)
+				i++;
+			
+		}
 	}
 }
