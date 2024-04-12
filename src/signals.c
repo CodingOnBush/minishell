3603cb13069182f943e4400c5f27189e95319d29
vvaudain <vvaudain@student.42.fr>
2024-04-12 16:44:03 +0200

    no double prompt when testing signals

--- src/signals.c
+++ src/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 12:45:26 by momrane           #+#    #+#             */
-/*   Updated: 2024/04/07 14:46:38 by momrane          ###   ########.fr       */
+/*   Updated: 2024/04/12 16:43:01 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	ft_handler(int signum)
 		printf("\n");
 		rl_replace_line("", STDIN_FILENO);// "ls |\n>"
 		rl_on_new_line();
-		rl_redisplay();
+		// rl_redisplay();
 	}
 }
 
