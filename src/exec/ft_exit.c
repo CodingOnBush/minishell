3bf7ed58d0d8095c29a3e7cdb9537a6577003eed
vvaudain <vvaudain@student.42.fr>
2024-04-23 15:06:03 +0200

    exit builtin issue fixed

--- src/exec/ft_exit.c
+++ src/exec/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vvaudain <vvaudain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 09:40:03 by ubuntu            #+#    #+#             */
-/*   Updated: 2024/04/23 12:30:41 by momrane          ###   ########.fr       */
+/*   Updated: 2024/04/23 15:05:30 by vvaudain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,37 @@ static long long	ft_atoll(char *str)
 	return (result * sign);
 }
 
+static int ft_is_too_big(char *str)
+{
+	if (ft_strlen(str) > 19)
+		return (YES);
+	if (ft_isnumber(str) == YES && ft_atoll(str) > LLONG_MAX)
+		return (YES);
+	return (NO);
+}
+
+static int	ft_arg_number(t_arg *arg_list)
+{
+	int	i;
+
+	i = 0;
+	while (arg_list != NULL)
+	{
+		i++;
+		arg_list = arg_list->next;
+	}
+	return (i);
+}
+
+static void ft_error(t_data *data, char *arg, char *str, int status)
+{
+	ft_putstr_fd("exit\nminishell: exit: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(str, 2);
+	ft_free_all(data);
+	exit(status);
+}
+
 int	ft_exit(t_data *data, t_arg *arg_list)
 {
 	int		status;
@@ -60,14 +91,10 @@ int	ft_exit(t_data *data, t_arg *arg_list)
 	if (arg_list->next != NULL)
 	{
 		str = arg_list->next->value;
-		if (ft_isnumber(str) == NO || (ft_isnumber(str) == YES && ft_atoll(str) > LLONG_MAX))
-		{
-			ft_putstr_fd("exit\nminishell: exit: ", 2);
-			ft_putstr_fd(str, 2);
-			ft_putstr_fd(": numeric argument required\n", 2);
-			ft_free_all(data);
-			exit(2);
-		}
+		if (ft_isnumber(str) == NO || ft_is_too_big(str) == YES)
+			ft_error(data, str, ": numeric argument required\n", 2);
+		else if (ft_arg_number(arg_list) > 2)
+			ft_error(data, str, ": too many arguments\n", 1);
 		status = ft_atoll(str) % 256;
 	}
 	ft_putstr_fd("exit\n", 1);
